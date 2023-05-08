b60204b55db666aaec5811b6b42561ae1041f4fb
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 21:52:29 +0200

    add audit

---
+++ src/XmlBazaPodataka/Tabele/AuditTabela.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace XmlBazaPodataka.Tabele
+{
+    public enum MessageType { [EnumMember] Info, [EnumMember] Warning, [EnumMember] Error };
+
+    [DataContract]
+    public class AuditTabela
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+
+        [DataMember]
+        public MessageType Message_Type { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+    }
+}

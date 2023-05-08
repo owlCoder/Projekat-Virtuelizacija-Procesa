3f948b510189feec337461d80a02750ef5699f96
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 23:13:55 +0200

    update

---
+++ src/Server/Interfejsi/IPreglediPotrosnje.cs
@@ -0,0 +1,20 @@
+﻿using Common.Izuzeci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Interfejsi
+{
+    // Interfejs koji predstavlja metode koje ce racunati pregled potrosnje (min, max, stand)
+    [ServiceContract]
+    public interface IPreglediPotrosnje
+    {
+        // Metoda koja pronalazi min, max, std potrosnju za tekuci datum
+        [OperationContract]
+        [FaultContract(typeof(PregledPotrosnjeIzuzetak))]
+        dynamic PregledPotrosnje();
+    }
+}

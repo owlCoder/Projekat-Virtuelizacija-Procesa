5d0b757e888b29ebf25094ff7126e3ff4afecefa
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 22:10:56 +0200

    base tables

--- src/XmlBazaPodataka/Tabele/AuditTabela.cs
+++ src/XmlBazaPodataka/Tabele/AuditTabela.cs
@@ -1,23 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace XmlBazaPodataka.Tabele
 {
+    // Enumerativni tip za potrebe upisa tipa greske odnosno belezenja tipa izvestaja
     public enum MessageType { [EnumMember] Info, [EnumMember] Warning, [EnumMember] Error };
 
+    // Klasa koja modeluje Audit tabelu datu ulaznom XML datotetom TBL_AUDIT.xml
     [DataContract]
     public class AuditTabela
     {
+        // Jedinstveni identifikator zapisa u XML tabeli
         [DataMember]
         public int Id { get; set; }
 
+        // Vremenski trenutak zapisa u XML tabeli
         [DataMember]
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; } 
 
+        // Koji tip poruke se upisuje u XML tabelu
         [DataMember]
         public MessageType Message_Type { get; set; }
 
+        // Detaljniji opis zapisa u XML tabeli
         [DataMember]
         public string Message { get; set; }
+
+        public AuditTabela()
+        {
+            // prazan konstruktor zbog serijalizacije
+        }
+
+        // Konstruktor sa parametrima
+        public AuditTabela(int id, DateTime timestamp, MessageType message_Type, string message)
+        {
+            Id = id;
+            Timestamp = timestamp;
+            Message_Type = message_Type;
+            Message = message;
+        }
+
+        // Metoda za poredjenje objekata klase AuditTabela
+        public override bool Equals(object obj)
+        {
+            return obj is AuditTabela tabela &&
+                   Id == tabela.Id &&
+                   Timestamp == tabela.Timestamp &&
+                   Message_Type == tabela.Message_Type &&
+                   Message == tabela.Message;
+        }
+
+        // Metoda za formatiran ispis objekta klase AuditTabela
+        public override string ToString()
+        {
+            // Izlazni format: [Info]: 2023-5-8 Uspesno izmeren podatak (ID: 102)
+            return string.Format("[{0}]: {1} {2} (ID: {3}", Message_Type, Timestamp.ToString(), Message, Id);
+        }
+
+        // Metoda za racunanje Hash vrednosti
+        public override int GetHashCode()
+        {
+            int hashCode = 795156360;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Timestamp.GetHashCode();
+            hashCode = hashCode * -1521134295 + Message_Type.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Message);
+            return hashCode;
+        }
     }
 }
---
+++ src/XmlBazaPodataka/Tabele/LoadTabela.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlBazaPodataka.Tabele
+{
+    [DataContract]
+    public class LoadTabela
+    {
+        // Jedinstevni identifikator merenja
+        [DataMember]
+        public int Id { get; set; }
+
+        // Vreme kada je merenje zabelezeno
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+
+        // Izmerena vrednost za dati trenutak
+        [DataMember]
+        public double MeasuredValue { get; set; }
+
+        public LoadTabela() 
+        { 
+            // Prazan konstruktor zbog serijalizacije
+        }
+
+        // Konstruktor sa parametrima
+        public LoadTabela(int id, DateTime timestamp, double measuredValue)
+        {
+            Id = id;
+            Timestamp = timestamp;
+            MeasuredValue = measuredValue;
+        }
+
+        // Metoda za proveru jednakosti objekata klase LoadTabela
+        public override bool Equals(object obj)
+        {
+            return obj is LoadTabela tabela &&
+                   Id == tabela.Id &&
+                   Timestamp == tabela.Timestamp &&
+                   MeasuredValue == tabela.MeasuredValue;
+        }
+
+        // Metoda za formatiran ispis objekta klase LoadTabela
+        public override string ToString()
+        {
+            // Izlazni format: [122]: 2023-5-8 = 12223.143
+            return string.Format("[{0}]: {1} = {2}", Id, Timestamp.ToString(), MeasuredValue);
+        }
+
+
+        // Metoda za generisanje Hask vrednosti
+        public override int GetHashCode()
+        {
+            int hashCode = 326352841;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Timestamp.GetHashCode();
+            hashCode = hashCode * -1521134295 + MeasuredValue.GetHashCode();
+            return hashCode;
+        }
+    }
+}

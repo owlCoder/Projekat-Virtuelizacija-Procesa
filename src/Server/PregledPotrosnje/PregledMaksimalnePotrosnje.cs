0f22b8681d89b9076005dbe63aae46c68c281616
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 23:25:00 +0200

    pregledi potrosnje

---
+++ src/Server/PregledPotrosnje/PregledMaksimalnePotrosnje.cs
@@ -0,0 +1,19 @@
+﻿using Server.Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.PregledPotrosnje
+{
+    public class PregledMaksimalnePotrosnje : IPreglediPotrosnje
+    {
+        // Metoda koja racuna maksimalnu potrosnju za tekuci dan
+        public dynamic PregledPotrosnje()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
@@ -0,0 +1,18 @@
+﻿using Server.Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.PregledPotrosnje
+{
+    public class PregledStandardneDevijacijePotrosnje : IPreglediPotrosnje
+    {
+        // Metoda koja racuna standardnu devijaciju potrosnje za tekuci dan
+        public dynamic PregledPotrosnje()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

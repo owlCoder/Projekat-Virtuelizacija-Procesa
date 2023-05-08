21076ffadfba05c8e4b458f65d8ee10bb00ef550
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 23:29:18 +0200

    done

---
+++ src/Server/StatistickiServis.cs
@@ -0,0 +1,16 @@
+﻿using Server.Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class StatistickiServis // : IPreglediPotrosnje
+    {
+        // TO DO
+        // Hint: Standardna implementacija ipregledipotrosnje
+        // a u njima se poziva PregledPotrosnje.Pregled Min/Max/Std klase
+    }
+}

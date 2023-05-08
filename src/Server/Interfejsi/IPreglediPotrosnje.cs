78bb6e92f65d2642d48111aafb2063f3489d9740
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-08 23:25:27 +0200

    remove unused usings

--- src/Server/Interfejsi/IPreglediPotrosnje.cs
+++ src/Server/Interfejsi/IPreglediPotrosnje.cs
@@ -1,10 +1,5 @@
 ﻿using Common.Izuzeci;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server.Interfejsi
 {
--- src/XmlBazaPodataka/XmlBazaPodatakaServis.cs
+++ src/XmlBazaPodataka/XmlBazaPodatakaServis.cs
@@ -1,6 +1,4 @@
-﻿using XmlBazaPodataka.Interfejsi;
-
-namespace XmlBazaPodataka
+﻿namespace XmlBazaPodataka
 {
     // Klasa koja implementira potrebne servise za rad sa Xml bazom podataka
     public class XmlBazaPodatakaServis // : IBazaPodataka, IXmlCsvFunkcije

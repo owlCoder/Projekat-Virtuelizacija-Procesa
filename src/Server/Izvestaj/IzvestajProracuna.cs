cebe0eaddd9860ff4c77c00e1b35136eeb394e2e
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-09 11:33:43 +0200

    make new interface

---
+++ src/Server/Izvestaj/IzvestajProracuna.cs
@@ -0,0 +1,19 @@
+﻿using Common.Modeli;
+using Server.Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Izvestaj
+{
+    public class IzvestajProracuna : IIzvestajProracuna
+    {
+        public Stream NapraviIzvestajNakonProracuna(List<Proracun> podaci)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

9201663baf37cf55719d0ec4876fabd8ab540414
Danijel Jovanović <danijel.xda@gmail.com>
2023-05-10 23:28:42 +0200

    complete comments

--- src/Klijent/Komande/IKomanda.cs
+++ src/Klijent/Komande/IKomanda.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Izuzeci;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -11,10 +12,24 @@ namespace Klijent.Komande
     [ServiceContract]
     public interface IKomanda
     {
-        // Metoda na osnovu lokacija direktorijuma csv datoteke iz App.config
-        // kreira
+        // Metoda na osnovu lokacija direktorijuma csv datoteka iz App.config
+        // kreira novi MemoryStream u kom je otvorena datoteka
+        // i taj memorijski tok prosledjuje servisu za parsiranje i obradu datoteke
+        // Odredisni EndPoint:
+        // XmlBazaPodataka -> bool ParsiranjeCsvDatoteke(MemoryStream csv, out List<Audit> greske);
+        // Povratna vrednost: true ako je uspesno kreiran MemoryStream i poslat na servis, u suprotnom false
         [OperationContract]
         [FaultContract(typeof(KomandaIzuzetak))]
         bool SlanjeCsv();
+
+
+        // Metoda koja salje get komandu na servis, ulazni parametri funkcije su podrazumevano
+        // false, dok se mogu istovremeno zahtevati i sve tri kalkulacije.
+        // Odrednisni EndPoint je Server -> StatistickiServer
+        // Povratna vrednost je true ako postoji barem ijedan zapis, u suprotnom false
+        // Na kraju ako postoji podataka, kreira se tekstualna datoteka koja sadrzi primljene proracune
+        [OperationContract]
+        [FaultContract(typeof (KomandaIzuzetak))]
+        bool SlanjeGetKomande(bool IsMin = false, bool IsMax = false, bool IsStand = false);
     }
 }

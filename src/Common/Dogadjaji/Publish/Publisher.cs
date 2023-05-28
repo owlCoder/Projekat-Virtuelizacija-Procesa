e54695142f4d31ea05f0f4ac04f0f2be041f0975
owlCoder <jovanovic.pr55.2020@uns.ac.rs>
2023-05-28 16:14:41 +0200

    move to common interface

---
+++ src/Common/Dogadjaji/Publish/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Dogadjaji.Publish
+{
+    [DataContract]
+    public class Publisher : IPublisher
+    {
+        public double Publish(bool IsMin, bool IsMax, bool IsStand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                }
+
+                // Dispose unmanaged resources.
+                disposed = true;
+            }
+        }
+    }
+}

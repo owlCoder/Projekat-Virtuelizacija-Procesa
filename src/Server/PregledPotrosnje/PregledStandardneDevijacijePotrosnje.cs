fa439cf6b5035509232d9f90ba0e0892661a3b80
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:48:56 +0200

    Refactor method for min calculation

--- src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
@@ -10,6 +10,15 @@ namespace Server.PregledPotrosnje
     public class PregledStandardneDevijacijePotrosnje : IPreglediPotrosnje
     {
         // Metoda koja racuna standardnu devijaciju potrosnje za tekuci dan
-        
+         public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
+        {
+            // Promenljiva u kojoj se cuva najmanja zabelezena potrosnja
+            double potrosnja = 0.0;
+
+            // pronaci najmanju vrednost potrosnje i upisati je u potrosnja
+            potrosnja = procitano_tekuci_dan.Any() ? procitano_tekuci_dan.Min(p => p.MeasuredValue) : 0.0;
+
+            return potrosnja;
+        }
     }
 }

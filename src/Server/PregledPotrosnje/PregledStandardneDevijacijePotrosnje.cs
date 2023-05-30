44a6ca5f68c5564b1f15a99092b05024ef84384d
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:49:45 +0200

    update deviation function

--- src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
@@ -10,15 +10,15 @@ namespace Server.PregledPotrosnje
     public class PregledStandardneDevijacijePotrosnje : IPreglediPotrosnje
     {
         // Metoda koja racuna standardnu devijaciju potrosnje za tekuci dan
-         public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
-        {
-            // Promenljiva u kojoj se cuva najmanja zabelezena potrosnja
-            double potrosnja = 0.0;
+          public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
+            {
+                // Promenljiva u kojoj se cuva standardna devijacija zabelezene potrosnje
+                double potrosnja = 0.0;
 
-            // pronaci najmanju vrednost potrosnje i upisati je u potrosnja
-            potrosnja = procitano_tekuci_dan.Any() ? procitano_tekuci_dan.Min(p => p.MeasuredValue) : 0.0;
+                // pronaci vrednost potrosnje po standardnoj devijaciji i upisati je u potrosnja
+                potrosnja = procitano_tekuci_dan.Any() ? new Devijacija().StandardnaDevijacija(procitano_tekuci_dan.Select(p => p.MeasuredValue)) : 0.0;
 
-            return potrosnja;
-        }
+                return potrosnja;
+            }
     }
 }

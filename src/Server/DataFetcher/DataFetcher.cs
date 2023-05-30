5628db86f74d8c8272f7a53592d3863933706ae9
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:43:13 +0200

    DataFetcher: get data from database

--- src/Server/DataFetcher/DataFetcher.cs
+++ src/Server/DataFetcher/DataFetcher.cs
@@ -12,7 +12,15 @@ namespace Server.PrikupljanjePodataka
         #region METODA ZA PRIKUPLJANJE PODATAKA IZ BAZE PODATAKA
         public IEnumerable<Load> PrikupiPodatkeZaTekuciDan()
         {
-            
+            // Povezivanje na server baze podataka
+            ChannelFactory<IBazaPodataka> kanal_xml_servis = new ChannelFactory<IBazaPodataka>("BazaPodataka");
+            IBazaPodataka proksi_xml = kanal_xml_servis.CreateChannel();
+
+            // Poziv metode koja ce procitati sve Load objekte za tekuci dan i smestiti u listu podataka
+            proksi_xml.ProcitajIzBazePodataka(out List<Load> procitano_tekuci_dan);
+
+            // vratiti procitane podatke pozivaocu metode
+            return procitano_tekuci_dan;
         }
         #endregion
     }

92e4e4935da134c7c472d35024773308fce01510
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:42:15 +0200

    update function sign

--- src/Server/PregledPotrosnje/PregledMaksimalnePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledMaksimalnePotrosnje.cs
@@ -11,9 +11,15 @@ namespace Server.PregledPotrosnje
     public class PregledMaksimalnePotrosnje : IPreglediPotrosnje
     {
         // Metoda koja racuna maksimalnu potrosnju za tekuci dan
-        public double PregledPotrosnje()
+        public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
         {
-            
+            // Promenljiva u kojoj se cuva najveca zabelezena potrosnja
+            double potrosnja = 0.0;
+
+            // pronaci najvecu vrednost potrosnje i upisati je u potrosnja
+            potrosnja = procitano_tekuci_dan.Any() ? procitano_tekuci_dan.Max(p => p.MeasuredValue) : 0.0;
+
+            return potrosnja;
         }
     }
 }

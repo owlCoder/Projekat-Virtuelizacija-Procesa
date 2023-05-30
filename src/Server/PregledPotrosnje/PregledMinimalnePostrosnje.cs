9a94bf83b5bc354c6078cb0bc3058b0f79d448f8
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:47:31 +0200

    update method to new one

--- src/Server/PregledPotrosnje/PregledMinimalnePostrosnje.cs
+++ src/Server/PregledPotrosnje/PregledMinimalnePostrosnje.cs
@@ -10,8 +10,11 @@ namespace Server.PregledPotrosnje
     public class PregledMinimalnePostrosnje : IPreglediPotrosnje
     {
         // Metoda koja racuna minimalnu potrosnju za tekuci dan
-        public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
+         public double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan)
         {
+            // Promenljiva u kojoj se cuva najmanja zabelezena potrosnja
+            double potrosnja = 0.0;
+
             
         }
     }

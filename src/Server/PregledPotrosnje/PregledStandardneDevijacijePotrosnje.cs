b962b093c0ec2b0ddd86c8dfdab2cedb8f1fbff6
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:48:16 +0200

    free up db connections

--- src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
@@ -18,14 +18,7 @@ namespace Server.PregledPotrosnje
             // Lista podataka u kojoj ce biti procitani podaci za tekuci dan
             List<Load> procitano_tekuci_dan = new List<Load>();
 
-            // Povezivanje na server baze podataka
-            ChannelFactory<IBazaPodataka> kanal_xml_servis = new ChannelFactory<IBazaPodataka>("BazaPodataka");
-            IBazaPodataka proksi_xml = kanal_xml_servis.CreateChannel();
-
-            // Poziv metode koja ce procitati sve Load objekte za tekuci dan i smestiti u listu podataka
-            proksi_xml.ProcitajIzBazePodataka(out procitano_tekuci_dan);
-
-            
+           
 
             return potrosnja_float;
         }

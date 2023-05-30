291ead355cbe8da428b1506c6de240226bc5e528
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:41:36 +0200

    migrate to double

--- src/Server/PregledPotrosnje/PregledMaksimalnePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledMaksimalnePotrosnje.cs
@@ -13,15 +13,7 @@ namespace Server.PregledPotrosnje
         // Metoda koja racuna maksimalnu potrosnju za tekuci dan
         public float PregledPotrosnje()
         {
-            // Promenljiva u kojoj se cuva najveca zabelezena potrosnja
-            float potrosnja_float = 0.0f;
-
-            // Lista podataka u kojoj ce biti procitani podaci za tekuci dan
-            List<Load> procitano_tekuci_dan = new List<Load>();
-
-           
-
-            return potrosnja_float;
+            
         }
     }
 }

825c5ce451603013d69c53d50184f3a7fc0945ab
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:46:45 +0200

    Update method

--- src/Server/PregledPotrosnje/PregledMinimalnePostrosnje.cs
+++ src/Server/PregledPotrosnje/PregledMinimalnePostrosnje.cs
@@ -12,12 +12,6 @@ namespace Server.PregledPotrosnje
         // Metoda koja racuna minimalnu potrosnju za tekuci dan
         public float PregledPotrosnje()
         {
-            // Promenljiva u kojoj se cuva najmanja zabelezena potrosnja
-            float potrosnja_float = 0.0f;
-
-            // Lista podataka u kojoj ce biti procitani podaci za tekuci dan
-            List<Load> procitano_tekuci_dan = new List<Load>();
-
             
             
 

543f49d4103e44cc2b4092dd6f78508ace275f4c
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:50:57 +0200

    add custom deviation class

---
+++ src/Server/Devijacija/Devijacija.cs
@@ -0,0 +1,20 @@
+using Server.Interfejsi;
+using System;
+using System.Collections.Generic;
+
+namespace Server.ProracunDevijacije
+{
+    #region KLASA ZA PRORACUN DEVIJACIJE
+    public class Devijacija : IDevijacija
+    {
+        #region PRAZAN KONSTRUKTOR
+        public Devijacija()
+        {
+            // Prazan konstruktor
+        }
+        #endregion
+
+        
+    }
+    #endregion
+}

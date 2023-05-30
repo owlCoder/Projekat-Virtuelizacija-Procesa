a0ed4f8b803d3b9dac8ee892dfeae0fb13ad2e4f
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:48:33 +0200

    remove old method for calculation

--- src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
+++ src/Server/PregledPotrosnje/PregledStandardneDevijacijePotrosnje.cs
@@ -10,17 +10,6 @@ namespace Server.PregledPotrosnje
     public class PregledStandardneDevijacijePotrosnje : IPreglediPotrosnje
     {
         // Metoda koja racuna standardnu devijaciju potrosnje za tekuci dan
-        public float PregledPotrosnje()
-        {
-            // Promenljiva u kojoj se cuva standardna devijacija zabelezene potrosnje
-            float potrosnja_float = 0.0f;
-
-            // Lista podataka u kojoj ce biti procitani podaci za tekuci dan
-            List<Load> procitano_tekuci_dan = new List<Load>();
-
-           
-
-            return potrosnja_float;
-        }
+        
     }
 }

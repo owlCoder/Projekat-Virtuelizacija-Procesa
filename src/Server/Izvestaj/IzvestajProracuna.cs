026c7d6a79c1c8e45629437108aa04ed6fc3ad18
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:39:37 +0200

    add list check

--- src/Server/Izvestaj/IzvestajProracuna.cs
+++ src/Server/Izvestaj/IzvestajProracuna.cs
@@ -10,7 +10,12 @@ namespace Server.Izvestaj
     {
         public MemoryStream NapraviIzvestajNakonProracuna(List<Proracun> podaci)
         {
-            throw new NotImplementedException();
+            // ako nema podataka - tj nije generisan nijedan proracun, izazvati izuzetak
+            if (podaci.ToList().Count == 0 || (podaci.ToList().FindAll(p => p.VrednostProracuna == -1)).Count == 3)
+            {
+                throw new FaultException<IzvestajIzuzetak>(
+                    new IzvestajIzuzetak("[ERROR]: Nema podataka za generisanje izvestaja!"));
+            }
         }
     }
 }

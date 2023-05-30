0e777e9fe2a254aac1edd58433d516954a045d68
sanja <jovisic.pr25.2020@uns.ac.rs>
2023-05-30 11:43:01 +0200

    Prepare datafetcher

---
+++ src/Server/DataFetcher/DataFetcher.cs
@@ -0,0 +1,20 @@
+using Common.Modeli;
+using Server.Interfejsi;
+using System.Collections.Generic;
+using System.ServiceModel;
+using XmlBazaPodataka.Interfejsi;
+
+namespace Server.PrikupljanjePodataka
+{
+    #region KLASA ZA PRIKUPLJANJE PODATAKA IZ XML BAZE PODATAKA
+    public class DataFetcher : IDataFetcher
+    {
+        #region METODA ZA PRIKUPLJANJE PODATAKA IZ BAZE PODATAKA
+        public IEnumerable<Load> PrikupiPodatkeZaTekuciDan()
+        {
+            
+        }
+        #endregion
+    }
+    #endregion
+}

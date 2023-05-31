c4d552f85b50afdeca53c6e846ac2320f7dc6fec
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:22:19 +0200

    add delegate

---
+++ src/Common/Dogadjaji/IInterakcijaDogadjajem.cs
@@ -0,0 +1,15 @@
+﻿using Common.Modeli;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Common.Dogadjaji
+{
+    [ServiceContract]
+    public interface IInterakcijaDogadjajem
+    {
+        #region METODA ZA IZVRSAVANJE DELEGATA
+        [OperationContract]
+        void Objavi(IEnumerable<Load> podaci);
+        #endregion
+    }
+}

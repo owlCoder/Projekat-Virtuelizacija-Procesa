abc11b5456f36b12feb4a472a091531c61276b26
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 14:10:29 +0200

    calculate standard deviation

--- src/Server/ProracunDevijacije/Devijacija.cs
+++ src/Server/ProracunDevijacije/Devijacija.cs
@@ -14,5 +14,24 @@ namespace Server.ProracunDevijacije
             // Prazan konstruktor
         }
         #endregion
+
+        #region METODA ZA PRORACUN STANDARDNE DEVIJACIJE
+        public double StandardnaDevijacija(IEnumerable<double> merenja)
+        {
+            double mean = 0.0;
+            double M2 = 0.0;
+            int count = 0;
+
+            foreach (double value in merenja)
+            {
+                count++;
+                double delta = value - mean;
+                mean += delta / count;
+                M2 += delta * (value - mean);
+            }
+
+            return Math.Sqrt(M2 / (count - 1));
+        }
+        #endregion
     }
 }

6a8ecc3cf12ad73332aae16c4f3c3a3d3358a836
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:39:56 +0200

    add calculation interface

---
+++ src/Common/Proracuni/IProracun.cs
@@ -0,0 +1,22 @@
+﻿using Common.Datoteke;
+using Common.Izuzeci;
+using System.ServiceModel;
+
+namespace Common.Proracuni
+{
+    #region INTERFEJS ZA MODELOVANJE POKRETANJA PRORACUNA NA SERVERU
+    [ServiceContract]
+    public interface IProracun
+    {
+        // Metoda koja kao ulazne parametre prima koje vrednosti proracuna
+        // klijent zeli da izvrsi za tekuci dan
+        // Povratna vrednost je tekstualna datoteka koja je kreirana od strane
+        // servisa i sadrzi preformatiran ispis proracuna
+        // U slucaju da za trenutni dan nisu zabelezeni podaci potrosnje
+        // ili potrosnju nije moguce izracunati desava se izuzetak PregledPotrosnjeIzuzetak
+        [OperationContract]
+        [FaultContract(typeof(PregledPotrosnjeIzuzetak))]
+        RadSaDatotekom PokreniProracun(bool IsMin, bool IsMax, bool IsStand);
+    }
+    #endregion
+}

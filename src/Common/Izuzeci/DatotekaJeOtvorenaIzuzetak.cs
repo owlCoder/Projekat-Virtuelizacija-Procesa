6e9a9110db1ee23ea1d60520e99c051da9d1b7a5
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:14:55 +0200

    add region

--- src/Common/Izuzeci/DatotekaJeOtvorenaIzuzetak.cs
+++ src/Common/Izuzeci/DatotekaJeOtvorenaIzuzetak.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace Common.Izuzeci
 {
+    #region KLASA ZA RAD SA IZUZETKOM OTVORENE DATOTEKE
     [DataContract]
     public class DatotekaJeOtvorenaIzuzetak
     {
+        #region POLJE KLASE
         [DataMember]
         public string Razlog { get; set; }
+        #endregion
 
+        #region KONSTRUKTOR KLASE
         public DatotekaJeOtvorenaIzuzetak(string razlog)
         {
             Razlog = razlog;
         }
+        #endregion
     }
+    #endregion
 }

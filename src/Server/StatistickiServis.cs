4eb3bffa6e222e0df5c9f131f70f48203527a76b
Andrea Čukić <cukic.pr145.2020@uns.ac.rs>
2023-06-01 00:55:23 +0200

    run static service

--- src/Server/StatistickiServis.cs
+++ src/Server/StatistickiServis.cs
@@ -9,6 +9,25 @@
         InterakcijaDogadjajem Interakcija = new InterakcijaDogadjajem();
         #endregion
 
+        #region METODA KOJA NA OSNOVU EVENT-A POKRENUTOG OD KLIJENTA POZIVA DELEGAT I KREIRA POKRECE IZVESTAJ PRORACUNA
+        public RadSaDatotekom PokreniProracun(bool IsMin, bool IsMax, bool IsStand)
+        {
+            // prikupljanje podataka iz baze podataka za tekuci dan
+            IEnumerable<Load> podaci = new DataFetcher().PrikupiPodatkeZaTekuciDan();
 
+            // Ako ne postoje uneti podaci za tekuci dan, ne desava se dogadjaj proracuna
+            // vec se izaziva izutetak PregledPotrosnjeIzuzetak
+            if (!podaci.Any())
+            {
+                throw new FaultException<PregledPotrosnjeIzuzetak>(
+                    new PregledPotrosnjeIzuzetak("[Error]: Nije zabelezena potrosnja za datum " + DateTime.Now.ToString("yyyy-MM-dd") + ". Unesite podatke potrosnje!"));
+            }
+
+            
+
+            // slanje datoteka na klijenta koji je zahtevao proracun
+            return generisana_datoteka;
+        }
+        #endregion
     }
 }

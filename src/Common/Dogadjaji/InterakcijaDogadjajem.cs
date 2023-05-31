31309c7f0b6077bfdc3d7361be4b992b102fccc3
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:29:16 +0200

    implement and add event

---
+++ src/Common/Dogadjaji/InterakcijaDogadjajem.cs
@@ -0,0 +1,36 @@
+﻿using Common.Modeli;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Common.Dogadjaji
+{
+    #region DELEGAT PRORACUNA
+    // Delegat ciji potpis odgovara metodi definisanoj u interfjesu IProracun
+    public delegate double ProracunDelegat(IEnumerable<Load> podaci);
+    #endregion
+
+    #region KLASA ZA UPRAVLJANJE INTERAKCIJOM DOGADJAJA I DELEGATA
+    // Klasa modeluje interakciju event i delegates
+    [DataContract]
+    public class InterakcijaDogadjajem : IInterakcijaDogadjajem
+    {
+        #region POLJA KLASE
+        // Dogadjaj na serveru koji ce se aktivirati pri odgovarajucoj akciji klijenta
+        public event ProracunDelegat IzvrsiProracun;
+
+        // Kolekcija podataka u kojoj ce se cuvati zahtevani proracuni
+        public List<Proracun> Proracuni;
+
+        // Niz tipova potrosnje koja se zahteva
+        public readonly string[] Tip = { "Max Load: ", "Min Load: ", "Stand Deviation: " };
+
+        public void Objavi(IEnumerable<Load> podaci)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+    }
+    #endregion
+}

9ac29f7eafb290507e92a9b989e6c8300cd6de1c
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 14:04:27 +0200

    deviation interface

---
+++ src/Server/Interfejsi/IDevijacija.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Server.Interfejsi
+{
+    #region INTERFEJS ZA PRORACUN DEVIJACIJE VREDNOSTI
+    public interface IDevijacija
+    {
+        // Metoda koja racuna standardnu devijaciju po formuli
+        // Ulazni parametar je lista merenja, dok je izlaz izracunata
+        // standardna devijacija
+        double StandardnaDevijacija(IEnumerable<double> merenja);
+    }
+    #endregion
+}

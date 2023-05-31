a3f0b2a59e8dd3fd6f4e802ad9babb57e19894c3
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:38:12 +0200

    overview of consumption interface

---
+++ src/Common/Potrosnja/IPreglediPotrosnje.cs
@@ -0,0 +1,18 @@
+﻿using Common.Izuzeci;
+using Common.Modeli;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Common.Potrosnja
+{
+    #region INTERFEJS KOJI PREDSTAVLJA METODE KOJE CE RACUNATI PREGLED POTROSNJE (MIN, MAX, STAND)
+    [ServiceContract]
+    public interface IPreglediPotrosnje
+    {
+        // Metoda koja pronalazi min, max, std potrosnju za tekuci datum
+        [OperationContract]
+        [FaultContract(typeof(PregledPotrosnjeIzuzetak))]
+        double PregledPotrosnje(IEnumerable<Load> procitano_tekuci_dan);
+    }
+    #endregion
+}

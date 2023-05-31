53b3dfe40ef16fd0694b03e0d08408d67186b1af
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 13:16:33 +0200

    clean up

--- src/Common/Izuzeci/KomandaIzuzetak.cs
+++ src/Common/Izuzeci/KomandaIzuzetak.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace Common.Izuzeci
 {
+    #region KLASA ZA RAD SA IZUZETKOM KOMANDE
     [DataContract]
     public class KomandaIzuzetak
     {
+        #region POLJE KLASE
         [DataMember]
         public string Razlog { get; set; }
+        #endregion
 
+        #region KONSTRUKTOR KLASE
         public KomandaIzuzetak(string razlog)
         {
             Razlog = razlog;
         }
+        #endregion
     }
+    #endregion
 }

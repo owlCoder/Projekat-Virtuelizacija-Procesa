862fd9b0d5dc1ab210e4c1baeb86d27a06500def
Katarina Marinković <ninikaca@gmail.com>
2023-05-31 14:03:40 +0200

    add data fetch

---
+++ src/Server/Interfejsi/IDataFetcher.cs
@@ -0,0 +1,15 @@
+﻿using Common.Modeli;
+using System.Collections.Generic;
+
+namespace Server.Interfejsi
+{
+    #region INTERFEJS KOJI MODELUJE METODU POTREBNU ZA PRIKUPLJANJE PODATAKA
+    public interface IDataFetcher
+    {
+        // Metoda se povezuje sa serverom xml baze podataka
+        // i prikuplja sve podatke za tekuci dan i smesta u kolekciju podataka
+        // Povratna vrednost metode je lista svih podataka merenja
+        IEnumerable<Load> PrikupiPodatkeZaTekuciDan();
+    }
+    #endregion
+}
